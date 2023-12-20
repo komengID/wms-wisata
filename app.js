cd9ed7f44a3c2c1e62186b6945ea78cc929a980a
Moh Qoming Zaki <mizakighun@gmail.com>
2023-12-20 11:20:55 +0700

    push dulu

--- app.js
+++ app.js
@@ -3,17 +3,15 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public', 'views'));
-app.set('css', path.join(__dirname, 'public', 'css'));
+app.set('views', path.join(__dirname, 'app', 'views'));
 
-app.get(/dashboard/, (req, res) => {
-    res.render('dashboard');
+app.get('/', (req, res) => {
+    res.render('index');
     }
-);
+);  
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
